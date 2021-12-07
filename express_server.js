1fe2406c321876a3f666252b5ca6189fe3de7e3f
vagrant <rah.leak@gmail.com>
2021-12-07 04:18:47 +0000

    adding a second route, edit

--- express_server.js
+++ express_server.js
@@ -32,5 +32,5 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render("urls_show", templateVars);
+  res.render("urls_shows", templateVars);
 });
